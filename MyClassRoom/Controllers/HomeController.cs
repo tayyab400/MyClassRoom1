7670235d8d8e24cb2b18d49499d4b8593767a571
tayyab400 <bcsf12m543@pucit.edu.pk>
2016-02-02 23:06:04 +0500

    New
    My new commit

---
+++ MyClassRoom/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyClassRoom.Models;
+
+namespace MyClassRoom.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public ActionResult gallery()
+        {
+            ViewBag.Title = "Gallery";
+            return View("gallery");
+        }
+        public ActionResult basicGrid()
+        {
+            ViewBag.Title = "Basic Grid";
+            return View("basicGrid");
+        }
+        public ActionResult fullWidth()
+        {
+            ViewBag.Title = "Full Width";
+            return View("fullWidth");
+        }
+        public ActionResult sideBarLeft()
+        {
+            ViewBag.Title = "Side Bar";
+            return View("sideBarLeft");
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult LoginConfirm(Login lg1)
+        {
+            var us = from m in db.Logins
+                         select m;
+           
+            if (!String.IsNullOrEmpty(lg1.user.ToString()))
+            {
+                us = us.Where(s => s.user.Contains(lg1.user.ToString()));
+                return View("Index");
+            }
+            return View("Login");
+            
+        }
+    }
+}
